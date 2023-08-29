dfd7c5c15bf5da6860f8eaabec606b4ee792160c
miguelissa <134017558+miguelissa@users.noreply.github.com>
2023-08-29 00:12:29 -0300

    agrego practico 2

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// declaramos las constantes y variables que usaremos
 const piedra=document.getElementById("piedra");
 const tijeras=document.getElementById("tijeras");
 const papel=document.getElementById("papel");
@@ -9,20 +10,31 @@ let ganador;
 let ronda =0;
 let ganoUsuario=0;
 let ganoCpu=0;
-let nombreJugador= prompt("ingrese el nombre del jugador:")
-nombreJugador=nombreJugador.toLocaleUpperCase();
+let nombreJugador;
 
+// funcion que toma el nombre del usuario y muestra la intefaz principal del juego
+function comenzar(){
+  nombreJugador=document.getElementById("name").value;
+  nombreJugador=nombreJugador.toLocaleUpperCase();
+  if (nombreJugador==""){
+    alert("Ingrese nombre valido")
+  } else{
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("juego").style.display="block";
+  }
+}
+// funcion que toma la eleccion del usuario y la aplica a una variable
 function eleccionUsuario(a){
   usuario=a;
 }
-
+// funcion que crea la opcion de la pc y la aplica a una variable
 function eleccionCpu(){
   let min=0;
   let max=2;
   let num=Math.floor(Math.random()*(max-min+1)+min);
   cpu = jugadas[num];
 }
-
+// funcion que determina el reglamento del juego
 function determinarGanador(a,b){
   if(a==="tijeras"&& b==="papel"){
       return a
@@ -38,17 +50,22 @@ function determinarGanador(a,b){
       return b;
   } 
 }
+// funcion que maneja la anotaciones de las rondas jugadas y los ganadores
 function marcador(){
   document.getElementById("ronda").innerHTML="Ronda: "+ronda;
   document.getElementById("puntosUsuario").innerHTML= nombreJugador + ": "+ganoUsuario;
   document.getElementById("puntosCpu").innerHTML= "Cpu: "+ganoCpu;
   if(ganoCpu==3){
-    document.getElementById("ganador").innerHTML= "Cpu Gano!";
+    document.getElementById("juego").style.display="none";
+    document.getElementById("perdedor").style.display="block";
+    document.getElementById("perdiste").innerHTML="Perdiste, intentalo nuevamente";
   }else if (ganoUsuario==3){
-    document.getElementById("ganador").innerHTML= nombreJugador +" GANO!";
+    document.getElementById("juego").style.display="none";
+    document.getElementById("ganador").style.display="block";
+    document.getElementById("ganaste").innerHTML="Felicidades "+nombreJugador+" sos el ganador!!";
   }
 }
-
+// funcion principal donde se realiza la jugada 
 function jugar(){
   if (usuario != "piedra" && usuario != "papel" && usuario != "tijeras"){
     alert("Seleccione opcion valida para jugar")
@@ -75,12 +92,15 @@ function jugar(){
     marcador();
   }
 }
-
+// funcion para el boton de reiniciar 
 function limpiar(){
   ronda=0;
   ganoCpu=0;
   ganoUsuario=0;
   usuario="";
+  document.getElementById("juego").style.display="block";
+  document.getElementById("ganador").style.display="none";
+  document.getElementById("perdedor").style.display="none";
   document.getElementById("textoUsuario").innerHTML="";
   document.getElementById("textoCpu").innerHTML="";
   document.getElementById("ganadorMano").innerHTML="";
@@ -89,9 +109,10 @@ function limpiar(){
   document.getElementById("puntosCpu").innerHTML="";
   document.getElementById("ganador").innerHTML="";
   botones.forEach(function (button){
-    button.classList.remove("selected");
+  button.classList.remove("selected");
   })
 }
+// funcion que selecciona la opcion elejida por el usuario 
 botones.forEach(function (button) {
   button.addEventListener("click", function () {
     botones.forEach(function (otherButton) {
